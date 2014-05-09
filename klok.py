7717d1d426e5a6fee296afe47dd3b7a7be633ccb
michielbrink <michielpa@gmail.com>
2014-05-09 22:06:15 +0200

    Update klok.py

--- klok.py
+++ klok.py
@@ -1,36 +1,30 @@
 #!/usr/bin/python
-import os, sys, time, serial
-
-portt='/dev/rfcomm5'
-
-executable = sys.executable
-args = sys.argv[:]
-args.insert(0, sys.executable)
-
-os.system("rfcomm connect rfcomm5 00:12:08:17:20:73 1 &")
+import os, time, serial, json
 
 try:
-  ser = serial.Serial(
-		port=portt,
-		baudrate=9600
-	)
+    ser = serial.Serial(port='/dev/ttyACM0',baudrate=9600)
 except Exception:
-		print "you have not port " + portt + " connected"
-		time.sleep(5)
-		os.execvp(executable, args)
+    print "cant connect"
 
-ser.open()
-ser.isOpen()
+data1 = ''
+fps = 0.05
 
-input=1
 while 1 :
-	# get keyboard input
-	input = raw_input(">> ")
 
-	# send the character to the device
-	try:
-		ser.write(input + '\r\n')
-	except Exception:
-		print "you have not port " + portt + " connected"
-		time.sleep(5)
-		os.execvp(executable, args)
+
+    data = [time.strftime('%I')[:1] ,time.strftime('%I')[1:] ,':' ,time.strftime('%M')[:1] ,time.strftime('%M')[1:] ,' ' ,time.strftime('%p')[:1] ,time.strftime('%p')[1:]]
+
+    data = ','.join(map(str,data))
+    data = '{'+data+'}'
+    if data != data1 :
+        ser.open()
+        ser.isOpen()
+        time.sleep(1)
+        for char in data:
+            ser.write(char)
+            time.sleep(fps)
+        ser.close()
+        data1 = data
+    time.sleep(1)
+
+
